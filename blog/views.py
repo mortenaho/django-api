54973c6d16c1dbfc7152bb9872314a1d30fe4552
Mortenaho <Mortenaho@DESKTOP-DFP1EAQ>
2020-12-04 22:48:30 +0330

    Build an api to receive posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core import serializers
+from .models import Author, Post
+import json
+# from django.http import JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World, I`m here")
+
+@csrf_exempt
+def posts(request):
+    post = Post.objects.all()
+    posts_serializers = serializers.serialize('json', post)
+    post_json = json.loads(posts_serializers)
+    data = json.dumps(post_json)
+    return HttpResponse(data)
